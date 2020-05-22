0cfee272472852da4469ee2dfa3ad569a6f8e5c7
Ple-zs <2506381226@qq.com>
2020-05-22 13:28:09 +0800

    更新了时间

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("2020-05-22 13:11");
         System.out.println("2020-05-22 13:22");
         System.out.println("2020-05-22 13:24");
+        System.out.println("2020-05-22 13:27");
     }
 }
